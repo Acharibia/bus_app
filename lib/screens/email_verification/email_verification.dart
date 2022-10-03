ab5497e0add9ad2208f94604ddc52c82f2a2f8f4
Your Name <you@example.com>
2022-10-03 10:25:30 +0000

    email verification

--- lib/screens/email_verification/email_verification.dart
+++ lib/screens/email_verification/email_verification.dart
@@ -1,3 +1,241 @@
+// ignore_for_file: unused_label
+
+import 'dart:js';
+
+import 'package:bus_app/components/rounded_button.dart';
+import 'package:bus_app/services/auth_response.dart';
+import 'package:bus_app/services/authentication_service.dart';
+import 'package:bus_app/services/main_model.dart';
+import 'package:bus_app/utils/util.dart';
+import 'package:email_auth/email_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+import '../signin/sign_screen.dart';
+import '../signup/signup_screen.dart';
+
+class VerifyEmailForm extends StatelessWidget {
+  VerifyEmailForm({Key? key}) : super(key: key);
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController nameEditingController = TextEditingController();
+  final TextEditingController emailEditingController = TextEditingController();
+   TextEditingController otpcontroller = new TextEditingController();
+  AutovalidateMode _autoValidate = AutovalidateMode.disabled;
+   GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey();
+    EmailAuth emailAuth = new EmailAuth(sessionName: "ShuttleTracker");
+
+  FocusNode _enterCodeFocusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(children: [
+        Text(
+          'Verify Your Email',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Text(
+          "Please enter the 6 digit code sent to your email",
+          style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Form(
+            key: _formKey,
+            child: Column(children: [
+              TextFormField(
+                focusNode: _enterCodeFocusNode,
+                autovalidateMode: _enterCodeFocusNode.hasFocus
+                    ? AutovalidateMode.onUserInteraction
+                    : AutovalidateMode.disabled,
+                controller: emailEditingController,
+                keyboardType: TextInputType.number,
+                autofocus: false,
+                enableSuggestions: false,
+                decoration: InputDecoration(
+                  hintText: "Enter Pincode",
+                  prefixIcon: Icon(
+                    Icons.email,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide:
+                        const BorderSide(width: 0, style: BorderStyle.none),
+                  ),
+                  filled: true,
+                  isDense: true,
+                  contentPadding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  fillColor: Colors.grey[300],
+                ),
+                //apply validation
+                validator: (code) {
+                  late String errorMessage;
+                  String pattern = r"^[1-9]+[0-9]*$";
+                  RegExp regex = RegExp(pattern);
+                  if (code!.isEmpty) {
+                    errorMessage = "code is required";
+                    return errorMessage;
+                  }
+                  if (!regex.hasMatch(code)) {
+                    errorMessage = "Enter a valid otp code";
+                    return errorMessage;
+                  }
+                  if (code.length != 6) {
+                    errorMessage = "Code must 6 numbers";
+                    return errorMessage;
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.end,
+              //   children: [
+              //     InkWell(onTap: () {
+              //       //   //open forgot password screen here
+              //       //   Navigator.pushAndRemoveUntil(
+              //       //       context,
+              //       //       MaterialPageRoute(
+              //       //         builder: (context) => const ForgotPasswordScreen(),
+              //       //       ),
+              //       //       (route) => false); //lets check
+              //       // },
+              //       child:
+              //       const Text("Resend code");
+              //     }),
+              //   ],
+              // ),
+              // const SizedBox(
+              //   height: 10,
+              // ),
+              RoundedButton(
+                  label: "CONFIRM CODE",
+                  onPressed: () {
+                    // // we will call firebase reset password by email method
+                    // AuthenticationService()
+                    //     .resetPassword(email: emailEditingController.text)
+                    //     .then((authResponse) {
+                    //   if (authResponse.authStatus == AuthStatus.success) {
+                    //     Util.showSuccessMessage(context,
+                    //         "Email has been sent to reset password, please check your mail id");
+                    //   } else {
+                    //     Util.showErrorMessage(context, authResponse.message);
+                    //   }
+                    // });
+                  })
+            ])),
+      ]),
+    );
+  }
+
+  Widget _buildSignUpButton() {
+    return ScopedModelDescendant<MainModel>(
+        builder: (BuildContext ctx, Widget? child, MainModel model) {
+      return RoundedButton(
+          label: "CONFIRM CODE",
+          onPressed: () {
+            verify(context);
+            // // we will call firebase reset password by email method
+            // AuthenticationService()
+            //     .resetPassword(email: emailEditingController.text)
+            //     .then((authResponse) {
+            //   if (authResponse.authStatus == AuthStatus.success) {
+            //     Util.showSuccessMessage(context,
+            //         "Email has been sent to reset password, please check your mail id");
+            //   } else {
+            //     Util.showErrorMessage(context, authResponse.message);
+            //   }
+            // });
+          });
+    });
+  }
+
+  void verify(context) {
+    var result = emailAuth.validateOtp(
+        recipientMail: AuthenticationService().getUserEmail().toString().trim(), userOtp: otpcontroller.text);
+    if (!result) {
+      // _scaffoldkey.currentState!.showSnackBar(SnackBar(
+      //   duration: Duration(seconds: 4),
+      //   backgroundColor: Colors.red,
+      //   content: Text("Email has been verified"),
+      // ));
+
+      // Navigator.pushAndRemoveUntil(
+      //     context,
+      //     MaterialPageRoute(builder: (context) => StudentMainScreen()),
+      //     (route) => false);
+    } else {
+      // _scaffoldkey.currentState!.showSnackBar(SnackBar(
+      //   duration: Duration(seconds: 4),
+      //   backgroundColor: Colors.red,
+      //   content: Text("Wrong OTP"),
+      // ));
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import 'package:define_app1/src/utils/app_colors.dart';
 // import 'package:define_app1/auth.config.dart';
 // import 'package:define_app1/src/StudentProfile/StudentMainScreen.dart';
